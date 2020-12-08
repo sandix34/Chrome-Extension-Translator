0b2e8c9ec2ed7843ba1ca334867071e502673f98
sandrine <sandrinezocca@gmail.com>
2020-12-08 14:27:45 +0100

    fetch API

--- content.js
+++ content.js
@@ -1,7 +1,27 @@
 function getUserSelection() {
   if (window.getSelection) {
     const selection = window.getSelection().toString();
-    console.log("selection", selection);
+
+    const payload = {
+      text: selection.trim(),
+      src: "en",
+      dest: "fr",
+      email: "frengly account email, enteredduring registration",
+      password: "frengly account",
+    };
+
+    // https://frengly.com/translate
+    fetch("https://frengly.com/frengly/data/translateREST", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(payload),
+    })
+    .then(data => data.json())
+    .then((res) => {
+      console.log(res);
+    });
   }
 }
 
